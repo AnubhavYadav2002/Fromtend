7cd7906d0b5828ff6bc1c6e3c51d1ef663f66d57
AnubhavYadav2002 <yadavanubhav250@gmail.com>
2024-11-11 11:11:09 +0530

    Integrated DISCORDCLONE/ into the Tailwindcss repository

---
+++ DISCORDCLONE/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
+
